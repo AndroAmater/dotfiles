01b134e4ff50d0cb4205c44f07ed5e1f751d5b0c
Andrej Fidel <andrej@hudlajf.com>
2023-05-08 15:54:59 +0200

    feat: Configure neovide settings

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -20,4 +20,12 @@ if not vim.g.vscode then
     vim.cmd("colorscheme material")
 
     vim.opt.guifont = { 'JetBrains Mono, NotoSansMono Nerd Font', ':h10' }
+
+    -- Configure neovide settings
+    if vim.g.neovide then
+        vim.g.neovide_cursor_animation_length = 0.05
+        vim.g.neovide_cursor_trail_size = 0.5
+        vim.g.neovide_cursor_antialiasing = true
+        vim.opt.guifont = { 'JetBrains Mono, NotoSansMono Nerd Font', ':h8' }
+    end
 end
