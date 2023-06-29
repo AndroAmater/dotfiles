a1b1e282cb737620ae9cfdcd99a29af5aa854c8e
Andrej Fidel <andrej@hudlajf.com>
2023-06-29 13:26:10 +0200

    feat: Replace leap with flash

--- nvim/lua/packageManagers/lazynvim.lua
+++ nvim/lua/packageManagers/lazynvim.lua
@@ -223,10 +223,52 @@ require("lazy").setup({
 		end,
 	},
 	{
-		"https://github.com/ggandor/leap.nvim.git",
-		config = function()
-			require("leap").add_default_mappings()
-		end,
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "o", "x" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+			{
+				"R",
+				mode = { "o", "x" },
+				function()
+					require("flash").treesitter_search()
+				end,
+				desc = "Flash Treesitter Search",
+			},
+			{
+				"<c-f>",
+				mode = { "c" },
+				function()
+					require("flash").toggle()
+				end,
+				desc = "Toggle Flash Search",
+			},
+		},
 	},
 	{
 		"ThePrimeagen/harpoon",
