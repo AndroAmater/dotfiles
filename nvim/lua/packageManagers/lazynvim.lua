5d108f200cc96c0144ba6f0a8b5ef89379f8d43f
Andrej Fidel <andrej@hudlajf.com>
2024-01-03 11:39:13 +0100

    feat: Update nvim lazy

--- nvim/lua/packageManagers/lazynvim.lua
+++ nvim/lua/packageManagers/lazynvim.lua
@@ -405,7 +405,7 @@ require("lazy").setup({
 				require("nvim-tree").setup({
 					sort_by = "case_sensitive",
 					view = {
-						width = 100,
+						width = 50,
 					},
 					filters = {
 						dotfiles = false,
@@ -426,19 +426,19 @@ require("lazy").setup({
 				})
 
 				-- Auto Open
-				local function open_nvim_tree(data)
-					-- buffer is a real file on the disk
-					local real_file = vim.fn.filereadable(data.file) == 1
+				-- local function open_nvim_tree(data)
+				-- 	-- buffer is a real file on the disk
+				-- 	local real_file = vim.fn.filereadable(data.file) == 1
 
-					-- -- buffer is a [No Name]
-					local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+				-- 	-- -- buffer is a [No Name]
+				-- 	local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
 
-					if not real_file and not no_name then
-						return
-					end
-					-- open the tree, find the file but don't focus it
-					require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
-				end
+				-- 	if not real_file and not no_name then
+				-- 		return
+				-- 	end
+				-- 	-- open the tree, find the file but don't focus it
+				-- 	require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
+				-- end
 
 				-- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 			end
