2c2fa768e9cffffa23307abad165af1546f286fd
Andrej Fidel <andrej@hudlajf.com>
2024-02-26 00:41:08 +0100

    fix: Formatting and linting on save

--- nvim/lua/formatOnSave/init.lua
+++ nvim/lua/formatOnSave/init.lua
@@ -39,58 +39,66 @@ local function setAutoFormat(pattern, formatters)
 end
 
 -- JavaScript/TypeScript
-setAutoFormat("*.js", {
-	F.prettier,
-	F.eslint,
-})
+-- setAutoFormat("*.js", {
+-- 	F.prettier,
+-- 	F.eslint,
+-- })
 
-setAutoFormat("*.ts", {
-	F.prettier,
-	F.eslint,
-})
+-- setAutoFormat("*.ts", {
+-- 	F.prettier,
+-- 	F.eslint,
+-- })
 
-setAutoFormat("*.json", {
-	F.prettier,
-})
+-- setAutoFormat("*.json", {
+-- 	F.prettier,
+-- })
 
-setAutoFormat("*.md", {
-	F.prettier,
-})
+-- setAutoFormat("*.md", {
+-- 	F.prettier,
+-- })
 
-setAutoFormat("*.yml", {
-	F.prettier,
-})
+-- setAutoFormat("*.yml", {
+-- 	F.prettier,
+-- })
 
-setAutoFormat("*.yaml", {
-	F.prettier,
-})
+-- setAutoFormat("*.yaml", {
+-- 	F.prettier,
+-- })
 
-setAutoFormat("*.vue", {
-	F.prettier,
-	F.eslint,
-})
+-- setAutoFormat("*.vue", {
+-- 	F.prettier,
+-- 	F.eslint,
+-- })
 
-setAutoFormat("*.html", {
-	F.prettier,
-})
+-- setAutoFormat("*.html", {
+-- 	F.prettier,
+-- })
 
-setAutoFormat("*.scss", {
-	F.prettier,
-})
+-- setAutoFormat("*.scss", {
+-- 	F.prettier,
+-- })
 
--- Lua
+-- -- Lua
 
-setAutoFormat("*.lua", {
-	F.stylua,
-})
+-- setAutoFormat("*.lua", {
+-- 	F.stylua,
+-- })
 
--- Go
-setAutoFormat("*.go", {
-	F.gofmt,
-})
+-- -- Go
+-- setAutoFormat("*.go", {
+-- 	F.gofmt,
+-- })
 
--- PHP
+-- -- PHP
 
-setAutoFormat("*.php", {
-	F.prettier,
+-- setAutoFormat("*.php", {
+-- 	F.prettier,
+-- })
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+augroup("__formatter__", { clear = true })
+autocmd("BufWritePost", {
+	group = "__formatter__",
+	command = ":FormatWrite",
 })
--- nvim/lua/packageManagers/lazynvim.lua
+++ nvim/lua/packageManagers/lazynvim.lua
@@ -551,6 +551,42 @@ require("lazy").setup({
 
 	-- LSP
 	{
+		"mhartington/formatter.nvim",
+		config = function()
+			require("formatter").setup({
+				logging = true,
+				log_level = vim.log.levels.WARN,
+				-- All formatter configurations are opt-in
+				filetype = {
+					lua = {
+						require("formatter.filetypes.lua").stylua,
+					},
+					javascript = {
+						require("formatter.filetypes.typescript").eslint_d,
+						require("formatter.filetypes.typescript").prettierd,
+					},
+					typescript = {
+						require("formatter.filetypes.typescript").eslint_d,
+						require("formatter.filetypes.typescript").prettierd,
+					},
+					vue = {
+						require("formatter.filetypes.typescript").eslint_d,
+						require("formatter.filetypes.typescript").prettierd,
+					},
+					json = {
+						require("formatter.filetypes.typescript").eslint_d,
+						require("formatter.filetypes.typescript").prettierd,
+					},
+					["*"] = {
+						-- "formatter.filetypes.any" defines default configurations for any
+						-- filetype
+						require("formatter.filetypes.any").remove_trailing_whitespace,
+					},
+				},
+			})
+		end,
+	},
+	{
 		"nvim-treesitter/nvim-treesitter",
 		lazy = false,
 		config = function()
@@ -728,9 +764,9 @@ require("lazy").setup({
 			-- (Optional) Configure lua language server for neovim
 			lsp.nvim_workspace()
 
-			-- require("lspconfig").volar.setup({
-			-- 	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
-			-- })
+			require("lspconfig").volar.setup({
+				filetypes = { "typescript", "javascript", "vue", "json" },
+			})
 
 			require("lspconfig").angularls.setup({
 				root_dir = require("lspconfig/util").root_pattern("nx.json", ".git") or vim.loop.os_homedir(),
